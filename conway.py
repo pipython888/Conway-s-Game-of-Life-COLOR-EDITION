fce01172a6fc0ef690fedd75f2b8e1e2c0466fed
Alexander Davison <alexanderdavison888@gmail.com>
2017-03-20 20:06:52 -0700

    Initial commit! :D

---
+++ conway.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+
+#### CONFIGUREABLE VALUES ####
+
+WIDTH = 500  # Screen width
+HEIGHT = 500  # Screen height
+CELL_SIZE = 10  # Cell size
+
+##############################
+
+# Setup
+
+screen = pygame.display.set_mode([WIDTH, HEIGHT])

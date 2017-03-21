2f2023a3d737d50d054c3c59bb4c3e624db7d2d6
Alexander Davison <alexanderdavison888@gmail.com>
2017-03-20 20:50:18 -0700

    Added base game loop and setup stuff

--- conway.py
+++ conway.py
@@ -13,3 +13,28 @@ CELL_SIZE = 10  # Cell size
 # Setup
 
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
+
+timer = pygame.time.Clock()
+
+# Colors
+YELLOW = (255, 255, 153)
+WHITE = (250, 250, 250)
+GREEN = (0, 250, 5)
+BLACK = (0, 0, 0)
+
+# Game loop
+done = False
+while not done:
+    # Event handlers
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    # Display graphics
+    pygame.display.update()
+
+    # Clear screen and tick
+    screen.fill(BLACK)
+    timer.tick(60)
+
+pygame.quit()
